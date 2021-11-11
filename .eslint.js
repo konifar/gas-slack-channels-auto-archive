00a515e12aa969725e3a00ff7db95c4858fc4c05
konifar <yahpeycoy0403@gmail.com>
2021-11-11 15:40:26 +0900

    add lint and test

---
+++ .eslint.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      plugins: ["@typescript-eslint", "import"],
+      rules: {
+        "no-unused-vars": "off",
+        "import/order": "error",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {varsIgnorePattern: "testGreeter"},
+        ],
+      },
+    },
+  ],
+};
