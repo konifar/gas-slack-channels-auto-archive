03af7cfbf5642a148b8534ab33f9210069a2b504
konifar <yahpeycoy0403@gmail.com>
2021-11-11 22:22:01 +0900

    refactor code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,42 @@
 module.exports = {
-  extends: ["eslint:recommended"],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  overrides: [
-    {
-      files: ["**/*.ts"],
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-      ],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        sourceType: "module",
-        project: "./tsconfig.json",
-      },
-      plugins: ["@typescript-eslint", "import"],
-      rules: {
-        "no-unused-vars": "off",
-        "import/order": "error",
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { varsIgnorePattern: "testPost" },
-        ],
-      },
-    },
+  root: true,
+  extends: [
+    // https://eslint.org/docs/rules/
+    "eslint:recommended",
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.ts
+    "plugin:@typescript-eslint/recommended",
+    // https://github.com/benmosher/eslint-plugin-import
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    // https://prettier.io/docs/en/integrating-with-linters.html
+    "prettier",
   ],
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  settings: {},
+  rules: {
+    "import/no-unresolved": "off", // cannot handle `paths` in tsconfig
+    "import/no-cycle": "error",
+    "import/no-default-export": "warn",
+
+    "@typescript-eslint/array-type": ["warn", {default: "generic"}],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {"selector": "default", "format": ["camelCase", "UPPER_CASE", "PascalCase"], "leadingUnderscore": "allow"},
+      {"selector": "typeLike", "format": ["PascalCase"], "leadingUnderscore": "allow"},
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
 };
--- src/slack.ts
+++ src/slack.ts
@@ -1,19 +1,210 @@
-import { WebClient } from "@slack/web-api";
+const SLACK_API_URL = "https://slack.com/api";
+const SLACK_TOKEN = PropertiesService.getScriptProperties().getProperty("SLACK_TOKEN");
+const SPREAD_SHEET_ID = PropertiesService.getScriptProperties().getProperty("SPREAD_SHEET_ID");
 
-const token =
-  PropertiesService.getScriptProperties().getProperty("SLACK_TOKEN");
-const web = new WebClient(token);
+function execute() {
+  const channels = getPublicChannels();
+  Logger.log(`Number of all channels: ${channels.length}`);
 
-const channelId =
-  PropertiesService.getScriptProperties().getProperty("TARGET_CHANNEL_ID");
+  const users = getAllUsers();
+  Logger.log(`Number of all users: ${users.size}`);
 
-function testPost() {
-  (async () => {
-    const res = await web.chat.postMessage({
-      channel: channelId,
-      text: "Hello there",
-    });
+  const sheetRowData = []; // Array to output to SpreadSheet
 
-    Logger.log(res.ts);
-  })();
+  for (let channel of channels) {
+    if (!channel.is_channel) {
+      continue;
+    }
+
+    const latestMessage = getLatestChannelMessage(channel.id);
+    const creatorName = users.get(channel.creator);
+
+    Logger.log(`  - Channel:#${channel.name}, Creator:@${creatorName}`);
+
+    if (latestMessage != null) {
+      // Calculate days diff from the last message date to the current date
+      const row = createSheetRow(channel, creatorName, users.get(latestMessage.user), latestMessage.ts, latestMessage.text, latestMessage.ts);
+      sheetRowData.push(row);
+    } else {
+      // Calculate days diff from the channel created date to the current date
+      const row = createSheetRow(channel, creatorName, "", channel.created, "", 0);
+      sheetRowData.push(row.toArray());
+    }
+  }
+
+  Logger.log(`Write data to SpreadSheet: ${sheetRowData.length}`);
+  writeDataToSpreadSheet(sheetRowData);
+}
+
+/**
+ * @return SpreadSheet row data array
+ */
+function createSheetRow(channel: any, creatorName: any, lastUserName: any, lastTs: number, lastMessageText: string, lastMessageTs: number): SheetRow {
+  const ellapsedDays = Math.floor((new Date().getTime() - new Date(lastTs * 1000).getTime()) / (1000 * 60 * 60 * 24));
+  const lastMessageDate = lastMessageTs > 0 ? formatDateYYYYMMddHHmmss(lastMessageTs) : "";
+  return new SheetRow(
+    channel.name,
+    channel.id,
+    creatorName,
+    formatDateYYYYMMddHHmmss(channel.created),
+    channel.num_members,
+    lastUserName,
+    lastMessageText,
+    lastMessageDate,
+    ellapsedDays,
+  );
+}
+
+class SheetRow {
+  channelName: string;
+  channelID: string;
+  creatorName: string;
+  channelCreatedDate: string;
+  channelNumMembers: number;
+  lastUserName: string;
+  lastMessageText: string;
+  lastMessageDate: string;
+  ellapsedDays: number;
+
+  constructor(
+    channelName: string,
+    channelID: string,
+    creatorName: string,
+    channelCreatedDate: string,
+    channelNumMembers: number,
+    lastUserName: string,
+    lastMessageText: string,
+    lastMessageDate: string,
+    ellapsedDays: number
+  ) {
+    this.channelName = channelName;
+    this.channelID = channelID;
+    this.creatorName = creatorName;
+    this.channelCreatedDate = channelCreatedDate;
+    this.channelNumMembers = channelNumMembers;
+    this.lastUserName = lastUserName;
+    this.lastMessageText = lastMessageText;
+    this.lastMessageDate = lastMessageDate;
+    this.ellapsedDays = ellapsedDays;
+  }
+
+  toArray(): Array<any> {
+    return [
+      this.channelName,
+      this.channelID,
+      this.creatorName,
+      this.channelCreatedDate,
+      this.channelNumMembers,
+      this.lastUserName,
+      this.lastMessageText,
+      this.lastMessageDate,
+      this.ellapsedDays,
+    ];
+  }
+}
+
+/**
+ * @return Array of all public channels
+ */
+function getPublicChannels(): Array<any> {
+  let channels: any[] = [];
+  let nextCursor = "";
+
+  while (true) {
+    let url = `${SLACK_API_URL}/conversations.list?token=${SLACK_TOKEN}&exclude_archived=true&types=public_channel&limit=999`;
+    if (nextCursor != "") {
+      url += `&cursor=${nextCursor}`
+    }
+    const json = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+    channels = channels.concat(json.channels);
+    if (json.response_metadata.next_cursor != "") {
+      nextCursor = json.response_metadata.next_cursor;
+      continue;
+    }
+    break;
+  }
+
+  return channels;
+}
+
+/**
+ * @return Map of user id and user name
+ */
+function getAllUsers(): Map<String, String> {
+  const usersMap = new Map<String, String>();
+  let nextCursor = "";
+
+  while (true) {
+    let url = `${SLACK_API_URL}/users.list?token=${SLACK_TOKEN}&limit=999`;
+    if (nextCursor != "") {
+      url += `&cursor=${nextCursor}`
+    }
+    const json = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+    for (let member of json.members) {
+      usersMap.set(member.id, member.name);
+    }
+    if (json.response_metadata.next_cursor != "") {
+      nextCursor = json.response_metadata.next_cursor;
+      continue;
+    }
+    break;
+  }
+  return usersMap;
+}
+
+function writeDataToSpreadSheet(rowData: Array<any>) {
+  if (SPREAD_SHEET_ID == null) {
+    return;
+  }
+
+  const sheet = SpreadsheetApp.openById(SPREAD_SHEET_ID).getActiveSheet();
+  if (sheet == null) {
+    return;
+  }
+
+  if (rowData.length == 0) {
+    Logger.log("Data is empty");
+    return;
+  }
+
+  // Clear data
+  sheet.getRange("A2:I1000").clearContent();
+
+  const rows = rowData.length;
+  const cols = rowData[0].length;
+  const range = sheet.getRange(2, 1, rows, cols);
+  range.setValues(rowData);
+  // Order by ellapsed date desc
+  range.sort([
+    {column: 9, ascending: false},
+    {column: 1, ascending: true},
+  ]);
+  sheet.setRowHeights(2, rows - 1, 21);
+}
+
+/**
+ * @return Latest message of the last 200 messages
+ */
+function getLatestChannelMessage(channelId: string) {
+  let url = `${SLACK_API_URL}/conversations.history?token=${SLACK_TOKEN}&channel=${channelId}&limit=200`;
+  const json = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+
+  for (let message of json.messages) {
+    // https://api.slack.com/events/message#subtypes
+    if (
+      message.type == "message"
+      && message.subtype != "channel_leave"
+      && message.subtype != "channel_join"
+    ) {
+      return message;
+    }
+  }
+  return null;
+}
+
+/**
+ * @return Formatted date string
+ */
+function formatDateYYYYMMddHHmmss(timestamp: number): string {
+  return Utilities.formatDate(new Date(timestamp * 1000), "Asia/Tokyo", "yyyy/MM/dd HH:mm:ss");
 }
